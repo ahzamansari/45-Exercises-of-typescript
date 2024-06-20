f54e7a75c67843655624f384e682b9f2f7435107
Ahzam Ansari <152402280+ahzamansari@users.noreply.github.com>
2024-06-20 20:39:20 +0500

    Add files via upload

---
+++ 25/main.ts
@@ -0,0 +1,9 @@
+//variable
+let aliencolor = "White";
+
+if(aliencolor === "Black"){
+    console.log("player just earned 5 points");
+};
+if(aliencolor === "White"){
+    console.log("Alien color is white");
+};
