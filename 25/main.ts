87da3f31a370d753212c108c874093a5d1c9a84a
Ahzam Ansari <152402280+ahzamansari@users.noreply.github.com>
2024-06-20 20:30:04 +0500

    Delete 25 directory

--- 25/main.ts
+++
@@ -1,9 +0,0 @@
-//variable
-let aliencolor = "White";
-
-if(aliencolor === "Black"){
-    console.log("player just earned 5 points");
-};
-if(aliencolor === "White"){
-    console.log("Alien color is white");
-};
