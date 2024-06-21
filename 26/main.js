d299a790b0b89d41346d4013e4398f9eec907da6
Ahzam Ansari <152402280+ahzamansari@users.noreply.github.com>
2024-06-21 15:16:05 +0500

    Add files via upload

---
+++ 26/main.js
@@ -0,0 +1,15 @@
+//Define variable
+var aliencolor = "green";
+if (aliencolor === "green") {
+    console.log("player just earned 5 point for shooting then alien");
+}
+else {
+    console.log("player just earned 10 points");
+}
+//Second version
+if (aliencolor === "blue") {
+    console.log("i am if statement");
+}
+else {
+    console.log("I am else statement");
+}
---
+++ 26/main.ts
@@ -0,0 +1,18 @@
+//Define variable
+let aliencolor = "green";
+
+if (aliencolor === "green"){
+    console.log("player just earned 5 point for shooting then alien");
+}
+else{
+    console.log("player just earned 10 points");
+}
+
+//Second version
+
+if(aliencolor === "blue"){
+    console.log("i am if statement");
+}
+else{
+    console.log("I am else statement");
+}
