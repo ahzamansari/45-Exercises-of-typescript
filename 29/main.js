a8edf98d3ff55e8d5180063f5e63f29e6fcd86f1
Ahzam Ansari <152402280+ahzamansari@users.noreply.github.com>
2024-06-21 20:46:21 +0500

    Add files via upload

---
+++ 29/main.js
@@ -0,0 +1,18 @@
+//Creating an array
+var favorite_fruit = ["Mango", "Strawberry", "Orange"];
+//% independent statement of if
+if (favorite_fruit.includes("Mango")) {
+    console.log("I Really Like Mango");
+}
+if (favorite_fruit.includes("Strawberry")) {
+    console.log("I Really Like Strawberry");
+}
+if (favorite_fruit.includes("Apple")) {
+    console.log("I Really Like Apple");
+}
+if (favorite_fruit.includes("Orange")) {
+    console.log("I Really Like Orange");
+}
+if (favorite_fruit.includes("Banana")) {
+    console.log("I Really like Banana");
+}
