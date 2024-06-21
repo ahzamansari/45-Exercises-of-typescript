2c7de379a6dd2b7ccf9513cf144c31b6040ba385
Ahzam Ansari <152402280+ahzamansari@users.noreply.github.com>
2024-06-21 20:26:17 +0500

    Add files via upload

---
+++ 27/main.ts
@@ -0,0 +1,42 @@
+// define variable
+let aliencolor = "green";
+
+console.log("version 1")
+//using If andd Else-If statement
+if(aliencolor === "green"){
+    console.log("you shot down green alien you have earned 5 points");   
+}
+else if(aliencolor === "yellow"){
+    console.log("you shot down yellow alien you have earned 10 points")
+}
+else if(aliencolor === "red"){
+    console.log("you shot down red alien you have earned 15 points")
+}
+
+//Version 2
+console.log("\nversion 2")
+let aliencolor2 = "yellow";
+
+if(aliencolor2 === "green"){
+    console.log("you shot down green alien you have earned 5 points");   
+}
+else if(aliencolor2 === "yellow"){
+    console.log("you shot down yellow alien you have earned 10 points")
+}
+else if(aliencolor2 === "red"){
+    console.log("you shot down red alien you have earned 15 points")
+}
+
+//Version 3
+console.log("\nversion 3")
+let aliencolor3 = "red";
+
+if(aliencolor3 === "green"){
+    console.log("you shot down green alien you have earned 5 points");   
+}
+else if(aliencolor3 === "yellow"){
+    console.log("you shot down yellow alien you have earned 10 points")
+}
+else if(aliencolor3 === "red"){
+    console.log("you shot down red alien you have earned 15 points")
+}
